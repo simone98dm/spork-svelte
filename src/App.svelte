ee4376a4046575a90d40a8ec77d6a2979dc16d88
simone98dm <simone.dalmas@outlook.it>
2021-11-18 21:30:00 +0100

    Bug fix, add theme change, improve stability

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,29 @@
 <script lang="ts">
   import Song from "./components/Song.svelte";
-  import type { Tracks } from "./models/Tracks";
-  const f = fetch(
-    "https://api.spotify.com/v1/me/top/tracks?time_range=long_term",
-    {
-      headers: {
-        Accept: "application/json",
-        Authorization:
-          "Bearer ",
-        "Content-Type": "application/json",
-      },
-    }
-  )
-    .then((x) => x.json() as unknown as Tracks)
-    .then((x) => x.items);
+  import { getTopTracks } from "./utils/utils";
+  function changeTheme() {
+    window.document.body.classList.toggle("dark");
+  }
 </script>
 
 <main>
-  <h1 class="page__title">Top Tracks All Time</h1>
-  {#await f then data}
-    {#each data as item, i}
-      <Song {item} />
+  <h1 class="page__title">
+    Your Top Tracks
+    <img
+      on:click={changeTheme}
+      alt="theme-choose"
+      src="https://img.icons8.com/fluency/48/000000/color-palette.png"
+    />
+  </h1>
+
+  {#await getTopTracks() then data}
+    {#each data as item, index}
+      <Song {item} {index} />
     {/each}
   {/await}
 </main>
 
-<style type="scss">
+<style lang="scss">
   @import url("https://fonts.googleapis.com/css2?family=Montserrat&Roboto&display=swap");
   :root {
     --font-family-primary: "Montserrat", serif;
@@ -37,14 +35,36 @@
     --line-height-text: 1;
     --font-size-caption: 14px;
     --line-height-caption: 1.2;
+  }
 
+  :global(body) {
     --color-text: #222022;
     --color-highlight-primary: #ffef7e;
     --color-highlight-secondary: #b7f9e9;
     --border-radius-primary: 32px;
+    --background-color: #e2e2e2;
+    --card-background-color: #dbdbdb;
+  }
+
+  :global(body.dark) {
+    --color-text: #e2e2e2;
+    --color-highlight-primary: #3886a5;
+    --color-highlight-secondary: #9954a7;
+    --border-radius-primary: 32px;
+    --background-color: #2b2b2b;
+    --card-background-color: #333033;
+  }
+
+  .page__title {
+    text-align: center;
+    font-weight: bolder;
+    color: var(--color-text);
+    font-family: var(--font-family-primary);
+    font-size: 48px;
   }
 
   main {
+    background-color: var(--background-color);
     display: grid;
     justify-content: center;
     align-items: center;
--- src/components/Song.svelte
+++ src/components/Song.svelte
@@ -1,118 +1,130 @@
 <script lang="ts">
   import type { Song } from "src/models/Tracks";
-
   export let item: Song;
+  export let index: number;
 </script>
 
 <div class="card">
   <img src={item.album.images[0].url} class="card__image" alt="brown couch" />
   <div class="card__content">
-    <!-- <p class="card__date">{i}</p> -->
+    <p class="card__date">{index + 1}</p>
     <span class="card__title">{item.name}</span>
     <div class="card__text">
       {#each item.artists as artist}
         <span>{artist.name}</span>
       {/each}
     </div>
+    <div class="card__text">
+      <a
+        class="spoty-btn spoty-btn-sm spoty-btn-primary"
+        href={item.external_urls.spotify}
+        target="_black">Open</a
+      >
+    </div>
   </div>
 </div>
 
+<style lang="scss">
+  @mixin mediaBig {
+    @media (min-width: 576px) {
+      @content;
+    }
+  }
 
-<style type="scss">
-    .page__title {
-      text-align: center;
-      font-weight: bolder;
-      font-family: var(--font-family-primary);
-      font-size: 48px;
+  @include mediaBig {
+    :root {
+      --font-size-title: 40px;
     }
-  
-    * {
-      -webkit-font-smoothing: antialiased;
-      -moz-osx-font-smoothing: grayscale;
-    }
-  
-    @mixin mediaBig {
-      @media (min-width: 576px) {
-        @content;
-      }
-    }
-  
+  }
+
+  .card {
+    max-width: 960px;
+    border-radius: var(--border-radius-primary);
+    box-shadow: 24px 24px 80px rgba(0, 0, 0, 0.1);
+    padding: 20px 20px 28px 20px;
+    box-sizing: border-box;
+    margin: 20px;
+    display: flex;
+    flex-direction: column;
+    background-color: var(--card-background-color);
+
     @include mediaBig {
-      :root {
-        --font-size-title: 40px;
-      }
+      flex-direction: row;
+      align-items: center;
+      margin: 40px;
+      padding: 32px;
     }
-  
-    .card {
-      max-width: 960px;
-      border-radius: var(--border-radius-primary);
-      box-shadow: 24px 24px 80px rgba(0, 0, 0, 0.1);
-      padding: 20px 20px 28px 20px;
-      box-sizing: border-box;
-      margin: 20px;
-      display: flex;
-      flex-direction: column;
-  
-      @include mediaBig {
-        flex-direction: row;
-        align-items: center;
-        margin: 40px;
-        padding: 32px;
-      }
+  }
+
+  .card__image {
+    width: 100%;
+    max-height: 300px;
+    border-radius: var(--border-radius-primary);
+    object-fit: cover;
+    margin-bottom: 18px;
+
+    @include mediaBig {
+      width: 45%;
+      max-height: none;
+      min-height: 400px;
+      margin-bottom: 0;
     }
-  
-    .card__image {
-      width: 100%;
-      max-height: 300px;
-      border-radius: var(--border-radius-primary);
-      object-fit: cover;
-      margin-bottom: 18px;
-  
-      @include mediaBig {
-        width: 45%;
-        max-height: none;
-        min-height: 400px;
-        margin-bottom: 0;
-      }
+  }
+
+  .card__content {
+    @include mediaBig {
+      width: 55%;
+      padding-left: 40px;
     }
-  
-    .card__content {
-      @include mediaBig {
-        width: 55%;
-        padding-left: 40px;
-      }
+  }
+
+  .card__text {
+    display: block;
+    font-family: var(--font-family-secondary);
+    font-size: var(--font-size-caption);
+    line-height: var(--line-height-caption);
+    text-transform: uppercase;
+    color: var(--color-text);
+    margin-bottom: 6px;
+    margin-top: 20px;
+
+    @include mediaBig {
+      margin-bottom: 8px;
     }
-  
-    .card__date {
-      display: block;
-      font-family: var(--font-family-secondary);
-      font-size: var(--font-size-caption);
-      line-height: var(--line-height-caption);
-      text-transform: uppercase;
-      color: var(--color-text);
-      margin-bottom: 6px;
-  
-      @include mediaBig {
-        margin-bottom: 8px;
-      }
-    }
-  
-    .card__title {
-      font-family: var(--font-family-primary);
-      font-size: var(--font-size-title);
-      line-height: var(--line-height-title);
-      color: var(--color-text);
-      box-decoration-break: clone;
-      background-image: linear-gradient(
-        90deg,
-        var(--color-highlight-primary),
-        var(--color-highlight-secondary)
-      );
-      background-size: 100% 42%;
-      background-repeat: no-repeat;
-      background-position: 0 85%;
-      padding: 0 4px;
-      margin-left: -4px;
-    }
-  </style>
-  
+  }
+
+  .card__title {
+    font-family: var(--font-family-primary);
+    font-size: var(--font-size-title);
+    line-height: var(--line-height-title);
+    color: var(--color-text);
+    box-decoration-break: clone;
+    background-image: linear-gradient(
+      90deg,
+      var(--color-highlight-primary),
+      var(--color-highlight-secondary)
+    );
+    background-size: 100% 42%;
+    background-repeat: no-repeat;
+    background-position: 0 85%;
+    padding: 0 4px;
+    margin-left: -4px;
+  }
+
+  .spoty-btn {
+    margin-bottom: 20px;
+    text-decoration: none;
+  }
+  .spoty-btn-sm {
+    font-size: 12px;
+    line-height: 1;
+    border-radius: 500px;
+    padding: 11px 32px 9px;
+  }
+  .spoty-btn-primary {
+    color: #fff;
+    background-color: #1db954;
+    min-width: 113px;
+    margin-right: 25px;
+  }
+</style>
